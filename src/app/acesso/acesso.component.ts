304b0dcf4dd7fde33739ad977f62105daf06a4bb
kairoLamarca <lamarca.kairo@gmail.com>
2018-01-05 14:08:50 -0200

    transição do painel com o login e cadastro
    transição do painel com o login e cadastroestadoPainel com animate e transition

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -14,12 +14,22 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
         style({ opacity: 0, transform: 'translate(-50px, 0px)'}),
         animate('500ms 0s ease-in-out') //duração, delay e aceleração
       ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({ opacity: 0, transform: 'translate(50px, 0px)'}),
+        animate('500ms 0s ease-in-out') //duração, delay e aceleração
+      ])
     ])
   ]
 })
 export class AcessoComponent implements OnInit {
 
   public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
   
   constructor() { }
 
