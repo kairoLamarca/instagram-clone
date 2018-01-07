d55bf6578ec204a8726cf7ae9afa638c73cdcd9b
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-01-06 22:51:09 -0200

    inclusão da model de usuario
    inclusão da model de usuario e populando a variavel no cadastrarUsuario

--- src/app/acesso/cadastro/cadastro.component.ts
+++ src/app/acesso/cadastro/cadastro.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+import { Usuario } from '../usuario.model';
+
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
@@ -26,7 +28,16 @@ export class CadastroComponent implements OnInit {
     this.exibirPainel.emit('login');
   }
 
-  public cadastrarUsuario(){
-    console.log(this.formulario);
+  public cadastrarUsuario() {
+    //console.log(this.formulario);
+
+    let usuario: Usuario = new Usuario(
+      this.formulario.value.email,
+      this.formulario.value.nome_completo,
+      this.formulario.value.nome_usuario,
+      this.formulario.value.senha
+    );
+
+    console.log(usuario);
   }
 }
