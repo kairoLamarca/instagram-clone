3f3d8618516165300ef28e491c57ba89da207669
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-01-08 22:31:43 -0200

    correção do firebase e método para salvar usuario no firebase
    correção do firebase, onde a versão 4.8.1 esta com erro e voltei para a 4.8.0 e método para salvar usuario no firebase com email e senha

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,7 +1,17 @@
 import { Usuario } from './acesso/usuario.model';
+import * as firebase from 'firebase';
 
 export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log('ta aqui', usuario);
+
+        //Cadastrar usuário no firebase
+        firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
+            .then((resposta: any) => {
+                console.log(resposta);
+            })
+            .catch((error: Error) => {
+                console.log(error);
+            })
     }
 }
