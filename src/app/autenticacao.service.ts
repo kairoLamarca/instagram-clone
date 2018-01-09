02ed6bb509d865f9ec2116aa1c0b55c2285b29e3
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-01-08 23:14:43 -0200

    egistrando dados complementares do usuario
    egistrando dados complementares do usuario com firebase,database

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -5,10 +5,22 @@ export class Autenticacao {
     public cadastrarUsuario(usuario: Usuario): void {
         console.log('ta aqui', usuario);
 
-        //Cadastrar usuário no firebase
+        //Cadastrar usuário no firebase na dimensão de autenticação
         firebase.auth().createUserWithEmailAndPassword(usuario.email, usuario.senha)
             .then((resposta: any) => {
-                console.log(resposta);
+
+                //remover a senha do atributo senha do objeto usuario
+                delete usuario.senha
+
+                //ref() referencia de onde vai criar o registro
+                //btoa -- converte a string em, base64
+                //push adiciona um novo documento
+                //set, apaga tudo e insere só um
+                //registrando dados complementares do usuario no path email base64 na dimensão de database
+                firebase.database().ref(`usuario_detalhe/${btoa(usuario.email)}`)
+                    //.push
+                    .set(usuario)
+
             })
             .catch((error: Error) => {
                 console.log(error);
