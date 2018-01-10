f232e6dc450f851337bfb517b9cc404da187df17
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-01-09 23:49:32 -0200

    utilização do getIdToken do firebase para retornar o token da autenticação
    utilização do getIdToken do firebase para retornar o token da autenticação do usuario atual

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -2,6 +2,9 @@ import { Usuario } from './acesso/usuario.model';
 import * as firebase from 'firebase';
 
 export class Autenticacao {
+
+    public token_id: string;
+
     public cadastrarUsuario(usuario: Usuario): Promise<any> {
         console.log('ta aqui', usuario);
 
@@ -28,10 +31,15 @@ export class Autenticacao {
     }
 
     public autenticar(email: string, senha: string): void {
-        console.log('email: ', email);
-        console.log('senha: ', senha);
         firebase.auth().signInWithEmailAndPassword(email, senha)
-            .then((resposta: any) => console.log(resposta))
+            .then((resposta: any) => {
+                firebase.auth().currentUser.getIdToken() //esse método recupera o token_id do processo de autenticação
+                    .then((idToken: string) => {
+                        this.token_id = idToken;
+
+                        console.log(this.token_id);
+                    })
+            })
             .catch((error: Error) => console.log(error))
     }
 }
