044ca3d7944367959486b3f5043db68ef1df118c
kairoLamarca <lamarca.kairo@gmail.com>
2018-01-11 15:34:13 -0200

    configurando o bd.service.ts
    configurando o bd.service.ts

--- src/app/home/incluir-publicacao/incluir-publicacao.component.ts
+++ src/app/home/incluir-publicacao/incluir-publicacao.component.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+import { Bd } from '../../bd.service';
+
 @Component({
   selector: 'app-incluir-publicacao',
   templateUrl: './incluir-publicacao.component.html',
   styleUrls: ['./incluir-publicacao.component.css']
 })
+
 export class IncluirPublicacaoComponent implements OnInit {
 
   public formulario: FormGroup = new FormGroup({
     'titulo': new FormControl(null)
   })
 
-  constructor() { }
+  constructor(
+    private bd: Bd
+  ) { }
 
   ngOnInit() {
   }
 
   public publicar(): void{
-    
+    this.bd.publicar();
   }
 
 }
